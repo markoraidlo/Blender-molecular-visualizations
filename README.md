9fba2f8558a0df66de0147623105b5c686b9dcaa
markoraidlo <marko.raidlo@gmail.com>
2022-05-26 01:56:21 +0300

    Lõplik esitus

--- README.md
+++ README.md
@@ -1,18 +1,19 @@
 # Blender molecular visualizations
 
+Github repository sisaldab Blenderi add-on moodulit vismol.py. Antud add-on-i saab kasutada molekulide 
+ja cube_to_iso.py abil saadud molekulaarorbitaalide isopindade visualiseerimiseks.
+
 ## Blender paketi lisamine
 
 Lae alla github repo ja paki lahti.
+
+Blenderi programmis:
 
 Edit -> Preferences -> Add-ons -> Install
 
 Vali vismol.py fail.
 
-(Võib olla töötab ka .zip faili installimine)
-
 ## Paketi eemaldamine arvutil
-
-
 
 Windows arvutil minna kausta
 
@@ -27,24 +28,84 @@ vismol.__file__
 ```
 
 ## Faili kasutamine:
+
+Add-on-i lisamisel Blenderisse ja selle importimisel pythoni terminalis
+
 ```
 import vismol
-
-vismol.create_molecule(file_path)
-```
-Näiteks:
-
-```
-vismol.create_molecule("C:\\Users\\Marko\\Desktop\\BlendProj\\blender-molecular-visualizations\\Molecules\\C6H6.xyz")
 ```
 
-Lisaks on võimalik kasutada argumente atom_radius ja bond_radius, mis on default väärtustega 0.8 ja 0.1.
+on võimalik paketi meetodeid välja kutsuda. Näiteks:
 
-Lisaks on võimalik kustutada molekuli käsuga
+
+```
+vismol.create_molecule("C:\\Users\\User_name\\Desktop\\\Molecules\\C6H6.xyz")
+```
+
+### Peamised meetodid
+
+```
+vismol.create_molecule(file_path, atom_radius = 0.8, bond_radius = 0.1)
+```
+
+Loob molekuli visualisatsiooni Blenderis. Võtab argumendiks .xyz faili asukoha.
+Valikulisteks argumenditeks on aatomi raaadius ja sideme raadius.
+
+```
+vismol.create_iso(file_path, scale = 1)
+```
+
+Loob isopinna visualisatsiooni Blenderis. Võtab argumendiks .csv faili, mille
+on loonud cube-to-iso.py fail.
+Valikuliseks argumendiks on skaala koefitsent, mis suurendab või vähendab
+loodava objekti mõõte.
+
 
 ```
 vismol.clear_collection()
 ```
+Kustutab kõik objektid, mateeriad ja kollektsioonid blenderi töölaual.  
+Soovitatav on kasutada enne iga uue molekuli või isopinna loomist.
+
+```
+vismol.add_bond(atom_1, atom_2, bond_radius = 0.1)
+```
+Kui programm ei suuda leida kõiki sidemeid, on neid võimalik antud meetodidga lisada.
+Meetod võtab argumenditeks kahe aatomi nimed Blenderi faili sees, milel vahele soovitakse sidet lisada.
+
+### cube-to-iso.py
+
+Programmi üheks osaks on .cube failis oleva skalaarvälja põhjal isopinna joonistamine.
+Projekti praeguses seisundis peab .cube faili väljaspool Blenderit töötlema ja siis selle create-iso() meetodiga
+sisse lugema. See on tingitud sellest, et Blenderi pythonil ei ole pip-i ja selle installimine ning sellega
+scikit-image installimine muudaks add-on-i paigaldamise tunduvalt keerukamaks.
+
+Kasutamiseks peab olema arvuti pythonil paigaldatud scikit-image versiooniga 0.19.2 või uuem. Pythoni faili 
+käivitamisel tuleb argumendiks anda sisse loetav .cube faili asukoht ning võib anda loodava .csv faili nimi.
+Programm loob uue faili, mis sisaldab isopinna tippe ning tahke.
+
+### Kõrval meetodid
+
+Kõrval meetodid on mõeldud peamiste meetodite töö lihtsustamiseks. Neid on võimalik soovi korral ka eraldi välja kutsuda.
 
 
-### Iga meetodi kohta eraldi rida ja seletus / Docs
+```
+vismol.read_atoms(file_path)
+```
+Loeb sisse aatomite andmed .xyz failist.
+
+```
+vismol.find_bonds(atoms)
+```
+Võtab sisse listi aatomite informatsiooniga ning tagastab sidemete listi.
+
+```
+vismol.read_iso(file_path)
+```
+def read_iso(file_path):
+Loeb sisse tippude ja tahkude .csv faili ning tagastab tippude ja tahkude np.ndarray-d.
+
+```
+vismol.create_mesh(name, verts, faces)
+```
+Loob Blenderi meshi, kasutades tippude ja tahkude np.ndarray-d.
--- cube_to_iso.py
+++ cube_to_iso.py
@@ -9,14 +9,14 @@
 # Failile võib anda lisa argumendiks kirjutatava faili nime, aga ei pea.
 #
 # Programm loeb .cube formaadi ning salvestab uude faili
-# loodud isopinna tipud ja tahud
+# skalraarväljast loodud isopinna tipud ja tahud.
 # ---------------------------------------------------------------------------
 
 import sys
 import os
 
 import numpy as np
-from skimage import measure
+from skimage import measure # Kasutada vähemalt 0.19.2 versiooni või uuemat
 
 
 args = sys.argv
@@ -29,6 +29,7 @@ elif len(args) > 3:
 file_path = args[1]
 print("Opening file {}".format(file_path))
 
+# Loeb .cube faili sisse
 with open(file_path,  'r') as file:
     # Faili nimi 
     cube_name = os.path.basename(file.name)
@@ -46,12 +47,12 @@ with open(file_path,  'r') as file:
     y = [float(x) for x in file.readline().split()]
     z = [float(x) for x in file.readline().split()]
 
-    #N aatomit
+    # N aatomit
     atoms = list()
     for i in range(N_atoms):
         atoms.append([float(x) for x in file.readline().split()])
     
-    #Cube andmed
+    # Cube andmed
     cube = np.zeros(int(x[0] * y[0] * z[0]))
     i = 0
     for line in file:
@@ -60,12 +61,6 @@ with open(file_path,  'r') as file:
             i += 1
             
 cube = np.reshape(cube, (int(x[0]), int(y[0]), int(z[0])))
-
-#TODO: Negative check?
-pos_and_neg = True
-
-# Pos
-#TODO: uurida default value 0.1
 verts, faces, normals, values = measure.marching_cubes(cube, 0.1)
 
 # Isopinna nullpunkti nihutamine
@@ -75,18 +70,20 @@ for i in range(len(verts_zeroed)):
     verts_zeroed[i][1] = verts_zeroed[i][1] - 0.5*y[0]
     verts_zeroed[i][2] = verts_zeroed[i][2] - 0.5*z[0]
     
-# Neg
-verts_neg, faces_neg, normals_neg, values_neg = measure.marching_cubes(cube, -0.1)
+# Negatiivse isopinna kontroll
+pos_and_neg = np.any(np.negative(cube) > 0.1)
+    
+# Negatiivse isopinna leidmine, kui on olemas
+if pos_and_neg:
+    verts_neg, faces_neg, normals_neg, values_neg = measure.marching_cubes(np.negative(cube), 0.1)
 
-# Isopinna nullpunkti nihutamine
-verts_zeroed_neg = verts_neg.copy()
-for i in range(len(verts_zeroed)):
-    verts_zeroed_neg[i][0] = verts_zeroed_neg[i][0] - 0.5*x[0]
-    verts_zeroed_neg[i][1] = verts_zeroed_neg[i][1] - 0.5*y[0]
-    verts_zeroed_neg[i][2] = verts_zeroed_neg[i][2] - 0.5*z[0]
-
-#TODO: Leia positiivsed ja negatiivsed eraldi ja salvesta eraldi failidesse
-
+    verts_zeroed_neg = verts_neg.copy()
+    for i in range(len(verts_zeroed_neg)):
+        verts_zeroed_neg[i][0] = verts_zeroed_neg[i][0] - 0.5*x[0]
+        verts_zeroed_neg[i][1] = verts_zeroed_neg[i][1] - 0.5*y[0]
+        verts_zeroed_neg[i][2] = verts_zeroed_neg[i][2] - 0.5*z[0]
+        
+# Uue faili nimi
 if len(args) == 3:
     csv_name = args[2]
 else:
@@ -94,46 +91,44 @@ else:
     
 print("Writing file: {}".format(csv_name))
     
+# Tipude ja tahkude salvestamine uude faili
 with open(csv_name,  'w') as file:
     if pos_and_neg:
         file.write("2\n")
     else: 
         file.write("1\n")
         
-    # Verts
+    # Positiivsed tipud
     file.write(str(verts_zeroed.size) + " " + str(verts_zeroed.shape[0]) + " " 
                + str(verts_zeroed.shape[1]) +'\n')
     for row in verts_zeroed:
         np.savetxt(file, row)
     
-    # Faces
+    # Positiivsed tahud
     file.write(str(faces.size) + " " + str(faces.shape[0]) + " " 
                + str(faces.shape[1]) +'\n')
     for row in faces:
         np.savetxt(file, row)
     
-    # Negatiivne pind
+    # Negatiivne isopind, kui on olemas
     if pos_and_neg:
-        # Verts
         file.write(str(verts_zeroed_neg.size) + " " + str(verts_zeroed_neg.shape[0]) 
                    + " " + str(verts_zeroed_neg.shape[1]) +'\n')
         for row in verts_zeroed_neg:
             np.savetxt(file, row)
         
-        # Faces
         file.write(str(faces_neg.size) + " " + str(faces_neg.shape[0]) 
                    + " " + str(faces_neg.shape[1]) +'\n')
         for row in faces_neg:
             np.savetxt(file, row)
 
     file.close()
-
-#Pos ja neg
-#Kommentaar
-#N - lines ver
-#...
-#Tühi
-#N - lines faces
-#...
-#Tühi
-#Kui neg siis neg verts and faces.
+    
+# Kirjutatava faili struktuur:
+#
+#Line 1 - 1 või 2, vastavalt kas on positiivsed või positiivsed ja negatiivsed isopinnad
+#Line 2 - Tippude ridade arv, tippude ndarray mõõdud
+#... - Tippude andmed
+#Line X - Tahkude ridade arv, tahkude ndarray mõõdud
+#... - Tahkude andmed
+#Line Y - Kui line 1 on 2, siis järgneb samas formaadis negatiivne isopind
